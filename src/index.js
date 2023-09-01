efaabaf2256e452f55c0d09aebe800422f19f352
rahem sorour <sorourrahem17@gmail.com>
2023-09-01 22:36:40 +0100

    completed assignement

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const getForm = document.querySelector('form');
+  const tasksList = document.getElementById('tasks');
+
+  getForm.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+
+    const taskInput = document.getElementById('new-task-description');
+    const taskDescription = taskInput.value.trim();
+
+    if (taskDescription !== '') {
+      const taskItem = document.createElement('li');
+      taskItem.innerHTML = `
+        ${taskDescription}
+        <button class="delete-button">Delete</button>
+      `;
+      tasksList.appendChild(taskItem);
+      console.log(tasksList)
+      taskInput.value = '';
+      const deleteButton = taskItem.querySelector('.delete-button');
+      deleteButton.addEventListener('click', function () {
+        taskItem.remove();
+      });
+    }
+  })
+
+  const restBtn=document.getElementById('restbuttom')
+  restBtn.addEventListener('click',()=>tasksList.innerHTML = '')
 });
+
